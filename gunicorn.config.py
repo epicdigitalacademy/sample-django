b9f4fa840e49c5a73b16227b0e94808ab6c01e8e
epicdigitalacademy <131464258+epicdigitalacademy@users.noreply.github.com>
2024-07-29 06:04:55 +0100

    Update gunicorn.config.py

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -3,6 +3,3 @@ import os
 
 def post_fork(server, worker):
     server.log.info("Worker spawned (pid: %s)", worker.pid)
-
-    resource = Resource.create(attributes={
-        "service.name": "api-service"
